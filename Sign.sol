b6418df13db41e379d36c5c27469c8f183d79d54
Neil Vinod <neilvinod14@gmail.com>
2018-08-17 09:47:16 -0700

    Sign.sol

---
+++ Sign.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.0;
+
+//...
+const SignVerifyArtifact = require('./contracts/SignAndVerifyExample')
+const SignVerify = contract(SignVerifyArtifact)
+SignVerify.setProvider(provider)
+//...
+SignVerify
+  .deployed()
+  .then(instance => {
+    let fixed_msg = `\x19Ethereum Signed Message:\n${msg.length}${msg}`
+    let fixed_msg_sha = web3.sha3(fixed_msg)
+    return instance.verify.call(
+      fixed_msg_sha,
+      v_decimal,
+      r,
+      s
+    )
+  })
+  .then(data => {
+    console.log('-----data------')
+    console.log(`input addr ==> ${addr}`)
+    console.log(`output addr => ${data}`)
+  })
+  .catch(e => {
+    console.error(e)
+  })
